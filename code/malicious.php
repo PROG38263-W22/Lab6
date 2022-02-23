a954c1a1f153e6d8c14ac3e0500abf9a267dc98b
PROG38263-W22 <98931946+PROG38263-W22@users.noreply.github.com>
2022-02-22 19:56:40 -0500

    Update malicious.php

--- code/malicious.php
+++ code/malicious.php
@@ -2,6 +2,6 @@
 <body>
 <p>Edit the code on this page to insert the IP of your virtual machine. Run this code from your host machine.</p>
 <img src="http://<your VM ip address goes here>/motd.php?motd=biscuits!&submit=Submit" 
-width="1" height="1">
+width="100" height="100">
 </body>
 </html>
